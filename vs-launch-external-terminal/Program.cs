cab3916fc9aeff79e2f0f1c6d322fa468c8eedfc
RaphaelT <raphael.tetreault@mohawkcollege.ca>
2025-09-26 15:26:30 -0400

    Add launch external terminal project

---
+++ vs-launch-external-terminal/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace vs_launch_external_terminal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Process process = new();
+
+            // Config
+            process.StartInfo.FileName = @"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe";
+            process.StartInfo.Arguments = "";
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            // Open in new window
+            process.StartInfo.UseShellExecute = true;
+
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
