f039875988e9199305b3a7a1bff4e835c39aec99
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2025-10-05 23:08:20 -0400

    Add dungeon crawler example

---
+++ DungeonCrawlerSample/MohawkTerminalGame/Classes/TerminalGrid.cs
@@ -0,0 +1,16 @@
+﻿namespace DungeonCrawlerSample
+{
+    /// <summary>
+    ///     
+    /// </summary>
+    public class TerminalGrid : TerminalGridBase<string>
+    {
+        public TerminalGrid(string defaultValue) : base(defaultValue)
+        {
+        }
+
+        public TerminalGrid(int width, int height, string defaultValue) : base(width, height, defaultValue)
+        {
+        }
+    }
+}
---
+++ DungeonCrawlerSample/MohawkTerminalGame/Enums/TerminalExecuteMode.cs
@@ -0,0 +1,12 @@
+﻿namespace DungeonCrawlerSample
+{
+    /// <summary>
+    ///     Execution mode for <see cref="TerminalGame"/>.
+    /// </summary>
+    public enum TerminalExecuteMode
+    {
+        ExecuteOnce,
+        ExecuteLoop,
+        ExecuteTime,
+    }
+}
