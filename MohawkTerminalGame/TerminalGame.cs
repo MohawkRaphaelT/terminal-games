88ba3523b06a964871cd191f98d921dbd4df7fc7
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2025-10-05 23:11:14 -0400

    Update terminal (reset color) and template

--- MohawkTerminalGame/TerminalGame.cs
+++ MohawkTerminalGame/TerminalGame.cs
@@ -8,15 +8,7 @@ public class TerminalGame
     /// Run once before Execute begins
     public void Setup()
     {
-        Program.TerminalExecuteMode = TerminalExecuteMode.ExecuteTime;
-        Program.TargetFPS = 60;
-
-        Terminal.SetTitle("Title");
-        Terminal.WriteWithWordBreaks = true; // prevent word breaks when wrapping text
-        Terminal.WordBreakCharacter = ' '; // break on space
-        Terminal.UseRoboType = false; // write out text one character at a time
-        Terminal.RoboTypeIntervalMilliseconds = 1; // timer per character
-        Terminal.CursorVisible = false; // if the cursor is visible
+        Program.TerminalExecuteMode = TerminalExecuteMode.ExecuteOnce;
     }
 
     // Execute() runs based on Program.TerminalExecuteMode (assign to it in Setup).
